09e4c8f90f79d4bd70c5c58a74d8dfe9ee8df010
Alexander Rykhlitskiy <sashkavalent@gmail.com>
2016-11-15 20:56:49 +0300

    Add youtube videos support

--- README.md
+++ README.md
@@ -1,21 +1,28 @@
-With this Coolness Measurement Tool you can measure coolness of audio parts you specify.
+With this **Coolness Measurement Tool** you can measure coolness of audio parts you specify.
 
-To use this tool you need [sox](http://sox.sourceforge.net/) to be installed.
+To use this tool you need [sox](http://sox.sourceforge.net/) to be installed. To download audio from YouTube you also need [youtube-dl](https://rg3.github.io/youtube-dl/).
 ## Usage example
+Providing youtube url
 ```bash
-ruby cmt.rb './samples/Финал Линди Хоп JnJ Open часть 1.mp3' 2:46-3:40 3:40-4:30 4:30-5:17 5:17-6:06 6:06-7:00 7:00-7:30 7:30-7:50 8:04-8:32 8:32-8:59 8:59-9:35
+ruby cmt.rb 'https://www.youtube.com/watch?v=ICz-edsOaFw' 2:46-3:40 3:40-4:30 4:30-5:17 5:17-6:06 6:06-7:00 7:00-7:30 7:30-7:50 8:04-8:32 8:32-8:59 8:59-9:35
 ```
+
 will give you
 ```
     period      coolness
-1   4:30-5:17   188
-2   3:40-4:30   186
-3   5:17-6:06   177
-4   8:32-8:59   176
-5   7:30-7:50   161
-6   6:06-7:00   161
-7   7:00-7:30   160
-8   8:04-8:32   150
-9   2:46-3:40   147
-10   8:59-9:35   145
+1   3:40-4:30   175
+2   4:30-5:17   169
+3   5:17-6:06   166
+4   8:32-8:59   165
+5   7:30-7:50   147
+6   6:06-7:00   144
+7   8:04-8:32   141
+8   7:00-7:30   141
+9   8:59-9:35   139
+10   2:46-3:40   131
+```
+
+Also you can provide local file path
+```bash
+ruby cmt.rb './samples/Финал Линди Хоп JnJ Open часть 1.mp3' 2:46-3:40 3:40-4:30 4:30-5:17 5:17-6:06 6:06-7:00 7:00-7:30 7:30-7:50 8:04-8:32 8:32-8:59 8:59-9:35
 ```
--- cmt.rb
+++ cmt.rb
@@ -1,11 +1,48 @@
 require 'fileutils'
+require 'uri'
 
-audio_file = ARGV[0]
+file_path = ARGV[0]
 time_periods = ARGV[1..-1]
+delete_audio_file = false
+
+if file_path =~ URI::regexp
+  puts "Analyzing video source..."
+
+  available_formats = `youtube-dl -F #{file_path}`
+  target_format = available_formats.split("\n").grep(/\s*m4a\s*/).first.split.first
+
+  puts "Downloading #{target_format} m4a audio file..."
+
+  download_log = `youtube-dl -f #{target_format} #{file_path}`
+  file_destination_marker = '[download] Destination: '
+  file_destination_log = download_log.split("\n").find { |s| s.start_with?(file_destination_marker) }
+  audio_file_path =
+    if file_destination_log
+      file_destination_log.gsub(file_destination_marker, '')
+    else
+      file_downloaded_marker = ' has already been downloaded'
+      file_downloaded_log = download_log.split("\n").find { |s| s.end_with?(file_downloaded_marker) }
+      file_downloaded_log.gsub(file_downloaded_marker, '').gsub('[download] ', '')
+    end
+
+  new_audio_file_path = audio_file_path + '.mp3'
+
+  puts 'Converting m4a to mp3...'
+
+  `ffmpeg -v 5 -y -i '#{audio_file_path}' -acodec libmp3lame -ac 2 -ab 192k '#{new_audio_file_path}'`
+  FileUtils.rm(audio_file_path)
+  delete_audio_file = true
+  audio_file_path = new_audio_file_path
+end
+
+audio_file_path ||= file_path
+
 periods_and_coolness = time_periods.map do |period|
+  puts "Analyzing #{period} period"
+
   start, ent = period.split('-')
-  file_name = "#{audio_file}_#{period}.mp3"
-  `sox '#{audio_file}' '#{file_name}' trim #{start} '=#{ent}'`
+  file_name = "#{audio_file_path}_#{period}.mp3"
+  `sox '#{audio_file_path}' '#{file_name}' trim #{start} '=#{ent}'`
 
   stat = `sox '#{file_name}' -n stat 2>&1`
   FileUtils.rm(file_name)
@@ -13,6 +50,7 @@ periods_and_coolness = time_periods.map do |period|
   mean_norm = stat.split("\n").grep(/\AMean\s*norm/).first.split.last.to_f
   [period, mean_norm]
 end
+FileUtils.rm(audio_file_path) if delete_audio_file
 
 puts "    period      coolness"
 periods_and_coolness.sort_by { |_period, coolness| -coolness }.each_with_index do |(period, coolness), index|
